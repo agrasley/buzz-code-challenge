49e569a64bbc47eec9e07870cdb231152d393ae1
Alex Grasley <grasley.pdx@gmail.com>
2023-09-18 12:53:21 -0700

    fetch flickr feed on server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/feed", async (req, res) => {
+  const result = await fetch(
+    "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1",
+  );
+  const feed = await result.json();
+  res.json(feed.items);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
